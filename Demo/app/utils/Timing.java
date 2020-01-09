e5869238e75d9714b30bda3977c7293c5c3aa366
viorelieremias <ieremiasviorel@yahoo.com>
2020-01-09 16:40:37 +0200

    - add clearer logging

--- Demo/app/utils/Timing.java
+++ Demo/app/utils/Timing.java
@@ -2,10 +2,14 @@ package utils;
 
 public class Timing {
     public String url;
+    public String start;
+    public String end;
     public long latency;
 
-    public Timing(String url, long latency) {
+    public Timing(String url, String start, String end, long latency) {
         this.url = url;
+        this.start = start;
+        this.end = end;
         this.latency = latency;
     }
 }
--- Demo/app/utils/TimingFactory.java
+++ Demo/app/utils/TimingFactory.java
@@ -6,6 +6,9 @@ import play.libs.ws.WSClient;
 import play.libs.ws.WSResponse;
 
 import javax.inject.Inject;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
@@ -13,16 +16,21 @@ public class TimingFactory implements WSBodyReadables, WSBodyWritables, TimingFa
     @Inject
     WSClient ws;
 
+    static final DateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS");
+
     public CompletableFuture<Timing> timedRequest(String url) {
         long start = System.currentTimeMillis();
+        String startStr = formatter.format(new Date(start));
 
         CompletableFuture<WSResponse> response = (CompletableFuture<WSResponse>) ws.url(url).get();
 
         return response.thenApply(new Function<WSResponse, Timing>() {
             @Override
             public Timing apply(WSResponse wsResponse) {
-                long latency = System.currentTimeMillis() - start;
-                return new Timing(url, latency);
+                long end = System.currentTimeMillis();
+                long latency = end - start;
+                String endStr = formatter.format(new Date(end));
+                return new Timing(url, startStr, endStr, latency);
             }
         });
     }
